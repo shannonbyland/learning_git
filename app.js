2b3ef756756fff6c2e7aa82c0b28bc35538d03e4
Shannon Byland <shannonbyland@gmail.com>
2017-04-20 20:22:03 -0600

    console logs in app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 alert('Welcome to Git!');
+
+console.log('we are on a new branch!');
+
+var firstName = 'Shannon';
+var lastName = 'Byland';
+
+console.log('Full Name: ' + firstName + ' ' + lastName);
