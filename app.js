4d20c211f59128502d2ae2a04652efb23181a6e0
Shannon Byland <shannonbyland@gmail.com>
2017-04-20 20:30:35 -0600

    age and if else

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ console.log('we are on a new branch!');
 
 var firstName = 'Shannon';
 var lastName = 'Byland';
+var age = 23;
 
 console.log('Full Name: ' + firstName + ' ' + lastName);
+
+if (age > 19)
+console.log('not a teen');
+else
+console.log('we are a teen. We hate our parents!');
